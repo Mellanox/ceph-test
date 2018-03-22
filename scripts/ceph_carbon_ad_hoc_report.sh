9a673b1feb93a70bb69fa0536bc4ad5ada9e7f17
Sasha Kotchubievsky <sashakot@mellanox.com>
2018-03-22 17:37:36 +0200

    ceph_carbon_ad_hoc_report.sh: Embed awk scriptwq
    Signed-off-by: Sasha Kotchubievsky <sashakot@mellanox.com>

--- scripts/ceph_carbon_ad_hoc_report.sh
+++ scripts/ceph_carbon_ad_hoc_report.sh
@@ -5,4 +5,30 @@ carbon_host=${carbon_host:-"dev-r-vrt-089"}
 carbon_port=${carbon_port:-2003}
 process_name=${process_name:-"ceph-osd"}
 
-pidstat -h -d 1 -u -C $process_name | grep --line-buffered -v '^$' | grep --line-buffered -v '^#' | grep --line-buffered -v '^Linux' | awk --assign=hostname=${hostname} '{ printf "servers.%s.pidstat.%s.read %s %s\nservers.%s.pidstat.%s.write %s %s\n", hostname, $6, $3, $1, hostname, $6, $4, $1 ; fflush(); }' #> /dev/tcp/${carbon_host}/${carbon_port}
+if cat /etc/ceph/ceph.conf | grep -v ";" | grep -q "async+rdma" ; then
+	ms_type="rdma"
+else
+	ms_type="tcp"
+fi
+
+################################
+# AWK scripts                  #
+################################
+read -d '' scriptVariable << 'EOF'
+    {
+	        print $6
+    }
+EOF
+
+################################
+# End of AWK Scripts           #
+################################
+
+pidstat -h -d 1 -u -C $process_name | \
+	grep --line-buffered -v '^$' | \
+	grep --line-buffered -v '^#' | \
+	grep --line-buffered -v '^Linux' | \
+	awk --assign=hostname=${hostname} --assign=app=${process_name} --assign=type=${ms_type} "$scriptVariable"
+#	'{ printf "ceph.%s.pidstat.%s.read %s %s \n servers.%s.pidstat.%s.write %s %s\n", hostname, $6, $3, $1, hostname, $6, $4, $1 ; fflush(); }' \
+#	awk "$scriptVariable"
+	#> /dev/tcp/${carbon_host}/${carbon_port}
