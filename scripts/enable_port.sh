6cb1f94c2da44f02db3b2019adcfc4622412fec4
Sasha Kotchubievsky <sashakot@mellanox.com>
2018-03-22 14:18:25 +0200

    enable_port.sh: Enable pfc
    Signed-off-by: Sasha Kotchubievsky <sashakot@mellanox.com>

--- scripts/enable_port.sh
+++ scripts/enable_port.sh
@@ -6,7 +6,7 @@ base=${HOSTNAME%%.*}
 echo $base
 id=${base##*0}
 echo $id
-interfaces=${interfaces:-"ens6f0 ib0 ib1"}
+interfaces=${interfaces:-"ens6f0"}
 
 function set_persistent_configuration()
 {
@@ -24,6 +24,16 @@ cat > /etc/sysconfig/network-scripts/ifcfg-${name} << EOF
 EOF
 }
 
+function enable_pfc()
+{
+	local name=$1
+	local ib=$(ibdev2netdev | grep $name | awk '{print $1}')
+
+	mlnx_qos -i $name --trust dscp
+	mlnx_qos -i $name --pfc 0,0,0,1,0,0,0,0
+	echo 106 > /sys/class/infiniband/$ib/tc/1/traffic_class
+}
+
 echo $interfaces
 
 n=$((1))
@@ -37,6 +47,7 @@ for interface in $interfaces; do
 	ifconfig $interface
 
 	set_persistent_configuration $interface $ip 16
+	enable_pfc $interface
 
 	n=$(($n+1))
 done
