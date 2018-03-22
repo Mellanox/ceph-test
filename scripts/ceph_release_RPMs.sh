6f3c07f629ddcbf0101ed77921bce3602b57385b
Sasha Kotchubievsky <sashakot@mellanox.com>
2018-03-22 14:19:13 +0200

    ceph_release_RPMs.sh: Add command line arguments
    Signed-off-by: Sasha Kotchubievsky <sashakot@mellanox.com>

--- scripts/ceph_release_RPMs.sh
+++ scripts/ceph_release_RPMs.sh
@@ -1,25 +1,72 @@
 #/bin/bash -ex
 
-if [ $# -lt 2 ] || [ "$1" == "-h" ]; then
-        echo this script releases RPMs from local ceph directory
-        echo you must provide:
-        echo -e "\t1. user name from your email, e.g. moshe@mellanox.com"
-        echo -e "\t2. latest version under http://gitbuilder.ceph.com/ceph-rpm-centos7-x86_64-basic/ref/"
-        echo 'USAGE:' $0 '"username" "v11.11.11"'
-        echo 'example:' $0 '"moshe" "v11.2.0"'
-        exit
+#if [ $# -lt 2 ] || [ "$1" == "-h" ]; then
+#        echo this script releases RPMs from local ceph directory
+#        echo you must provide:
+#        echo -e "\t1. user name from your email, e.g. moshe@mellanox.com"
+#        echo -e "\t2. latest version under http://gitbuilder.ceph.com/ceph-rpm-centos7-x86_64-basic/ref/"
+#        echo 'USAGE:' $0 '"username" "v11.11.11"'
+#        echo 'example:' $0 '"moshe" "v11.2.0"'
+#        exit
+#fi
+
+OPTS=$(getopt -o hv:d:r: --long help,version:,destination:,rpmtop: -- "$@")
+eval set -- "$OPTS"
+
+HELP=false
+DESTINATION=${DESTINATION:-"/auto/mswg/release/storage/ceph"}
+USER=${SUDO_USER:-`whoami`}
+VER=""
+RPMTOP=${RPMTOP:-`sudo -u $USER rpm --eval "%{_topdir}"`}
+
+echoerr()
+{
+	>&2 echo "$@"
+	exit 1
+}
+
+while true; do
+	case "$1" in
+		-v | --version ) VER=$2; shift; shift ;;
+		-d | --destination ) DESTINATION=$2; shift; shift ;;
+		-r | --rpmtop ) RPMTOP=$2; shift; shift ;;
+		-h | --help )    HELP=true; shift ;;
+		-- ) shift; break ;;
+		* ) break ;;
+	  esac
+done
+
+if [[ $HELP == true ]]; then
+	echo "This script releases RPMs from local ceph directory"
+	echo "Default destination folder: $DESTINATION"
+	echo "Owner of this folder is \"ceph-release\" user"
 fi
 
-DESTINATION=${DESTINATION:-"/auto/mswg/release/storage/ceph"}
-RPMTOP=${3:-`rpm --eval "%{_topdir}"`}
-VER=`git describe`               # example: v11.1.0-6089-g7a3e505
+if [[ -z $VER ]]; then
+	git rev-parse --is-inside-work-tree &&  VER=`git describe`               # example: v11.1.0-6089-g7a3e505
+
+	if [[ -z $VER ]]; then
+		echoerr "Either run the script from git repository or provide version number"
+	fi
+fi
+
+if [[ ! -d ${DESTINATION} ]]; then
+	echoerr "${DESTINATION} doesn't exist"
+fi
+
+if [[ ! -d ${RPMTOP} ]]; then
+	echoerr "${RPMTOP} doesn't exist"
+fi
+
 TARGET=${DESTINATION}/rpm-$VER
 
 echo "Source RPM top dir: $RPMTOP"
 echo "Version: $VER"
 echo "Destination: ${DESTINATION}"
 echo "Target: ${TARGET}"
+echo "User: $USER"
 
+exit 1
 
 function create_release
 {
