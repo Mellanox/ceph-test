b3cffc2caa699e5ece0d7cf62615c88a0fa4e97a
Sasha Kotchubievsky <sashakot@mellanox.com>
2018-03-27 23:10:54 +0300

    Add installation script for reporting service
    Signed-off-by: Sasha Kotchubievsky <sashakot@mellanox.com>

---
+++ scripts/mlnx-ceph-install-report-service.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+dir=$( cd "$(dirname "$0")" ; pwd -P )
+target=${target:-"/opt/mellanox/ceph/reporting-service"}
+source=${source:-"$dir/reporting-service"}
+name="mlnx-ceph-report"
+
+mkdir -p ${target}
+cp ${source}/mlnx-ceph-* ${target}
+
+cp ${source}/$name.service /etc/systemd/system
+systemctl daemon-reload
+systemctl start $name
+systemctl enable $name
+systemctl status $name
+
